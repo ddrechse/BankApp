f6adee12d8944754ad9b1f3b4a0e4438c3dcf707
ddrechse <doug.drechsel@oracle.com>
2022-06-15 11:22:53 -0400

    Add Dequeue, Rewrite enque/deque to use Oracle Node APIs

--- cloud/bankapp.js
+++ cloud/bankapp.js
@@ -1,6 +1,8 @@
-initialized = false
+const oracledb = require('oracledb')
+isInitialized = false;
 
 Parse.Cloud.beforeSave("BankAccount", async request => {
+
     const action = request.object.get("action");
     const amount = request.object.get("amount");
     const accountNum = request.object.get("accountNum");
@@ -111,6 +113,7 @@ Parse.Cloud.define('history', async req => {
       });
 
 Parse.Cloud.afterSave("BankAccount", async (request) => {
+
         const action = request.object.get("action");
         const toAccountNum = request.object.get("toAccountNum")
         const externalAccountNum = request.object.get("externalAccountNum")
@@ -149,64 +152,54 @@ Parse.Cloud.afterSave("BankAccount", async (request) => {
         }
       });
 
-    async function publish(message_content) {
-        const oracledb = require('oracledb')
-        const tnsnames = "/wallet"
-    
-    
-        oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
-        if(initialized === false) {
-            oracledb.initOracleClient({configDir: tnsnames});
-            
-            await runSQL(`
-            declare
-                already_exists number;
-            begin 
-                select count(*)
-                into already_exists
-                from user_queues
-                where upper(name) = 'PUBLISH';
-                if already_exists = 0 then  
-                    DBMS_AQADM.CREATE_QUEUE_TABLE (
-                    queue_table => 'publish_queuetable',
-                    queue_payload_type  => 'RAW');   
-                    DBMS_AQADM.CREATE_QUEUE ( 
-                    queue_name => 'publish',
-                    queue_table => 'publish_queuetable');  
-                    DBMS_AQADM.START_QUEUE ( 
-                    queue_name => 'publish');
-                end if;
-            end;`)
-    
-    
-        initialized = true;
-        }
-    
-        await runSQL(`
-        DECLARE
-            enqueue_options     dbms_aq.enqueue_options_t;
-            message_properties  dbms_aq.message_properties_t;
-            message_handle      RAW(16);
-            message             RAW(4096); 
-        
-        BEGIN
-            message :=  utl_raw.cast_to_raw('` + message_content + `'); 
-            DBMS_AQ.ENQUEUE(
-                queue_name           => 'publish',           
-                enqueue_options      => enqueue_options,       
-                message_properties   => message_properties,     
-                payload              => message,               
-                msgid                => message_handle);
-            COMMIT;
-        END;`)
+    async function initialize() {
+      const tnsnames = "/wallet"
+
+      oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+      if(isInitialized === false) {
+          oracledb.initOracleClient({configDir: tnsnames});
+          await runSQL(`
+          declare
+              already_exists number;
+              subscriber     sys.aq$_agent;
+          begin 
+              select count(*)
+              into already_exists
+              from user_queues
+              where upper(name) = 'PUBLISH';
+              if already_exists = 0 then                  
+                  DBMS_AQADM.CREATE_QUEUE_TABLE ( queue_table    => 'publish_queuetable',     queue_payload_type  => 'RAW', multiple_consumers => TRUE); 
+                  DBMS_AQADM.CREATE_QUEUE       ( queue_name     => 'publish',                queue_table         => 'publish_queuetable');
+                  DBMS_AQADM.START_QUEUE        (queue_name => 'publish');
+
+                  --- Create the subscriber agent
+                  subscriber := sys.aq$_agent('BankA_subscriber', NULL, NULL);
+                  DBMS_AQADM.ADD_SUBSCRIBER(queue_name => 'publish',   subscriber => subscriber);
+              end if;
+          end;`)
+          isInitialized = true;
+      }
     }
 
+    async function publish(message_content) {
+        await initialize();
+        await enquemsg(message_content);
+    }
+
+    async function checkTransfersQueue() {
+        await initialize();
+        const msg = await dequemsg();
+        return msg;
+    }
+
+
     async function runSQL(statement) {
-        const oracledb = require('oracledb')
     
         const user = "user";
         const password = "password";
         const connectString = "database";
+        console.log(statement);
+
         let connection;
         try {
             connection = await oracledb.getConnection({
@@ -215,8 +208,7 @@ Parse.Cloud.afterSave("BankAccount", async (request) => {
                 connectString: connectString
             });
             const result = await connection.execute(statement);
-            console.log(result.rows);
-            return result.rows
+            return result
         } catch (err) {
             console.log(err);
         } finally {
@@ -228,4 +220,84 @@ Parse.Cloud.afterSave("BankAccount", async (request) => {
                 }
             }
         }
-      }
+     }
+
+      async function enquemsg(message_content) {  
+    
+        const user = "user";
+        const password = "password";
+        const connectString = "database";
+
+          let connection;
+          try {
+            connection = await oracledb.getConnection({
+                user: user,
+                password: password,
+                connectString: connectString
+            });
+            const queueName = "publish";
+            const queue = await connection.getQueue(queueName);
+            await queue.enqOne(message_content);
+            await connection.commit();
+            return connection
+          } catch (err) {
+            console.log(err);
+          } finally {
+            if (connection) {
+              try {
+                  await connection.close();
+              } catch (err) {
+                  console.log(err);
+              }
+            }
+          }
+        }
+
+
+        async function dequemsg() {  
+      
+          const user = "user";
+          const password = "password";
+          const connectString = "database";
+
+            let connection;
+            try {
+              connection = await oracledb.getConnection({
+                  user: user,
+                  password: password,
+                  connectString: connectString
+              });
+              const queueName = "publish";
+              const queue = await connection.getQueue(queueName);
+              queue.deqOptions.consumerName = "BankA_subscriber";
+              queue.deqOptions.mode = oracledb.AQ_DEQ_MODE_REMOVE;
+              queue.deqOptions.wait = oracledb.AQ_DEQ_NO_WAIT;
+              queue.deqOptions.navigation = oracledb.AQ_DEQ_NAV_FIRST_MSG;
+              const msg = await queue.deqOne();
+              await connection.commit();
+              return msg.payload.toString();
+            } catch (err) {
+              console.log(err);
+            } finally {
+              if (connection) {
+                try {
+                    await connection.close();
+                } catch (err) {
+                    console.log(err);
+                }
+              }
+            }
+          }        
+      
+
+
+      Parse.Cloud.job("checkForTransfers", async (request) =>  {
+        // params: passed in the job call
+        // headers: from the request that triggered the job
+        // log: the ParseServer logger passed in the request
+        // message: a function to update the status message of the job object
+        const { params, headers, log, message } = request;
+
+        result = await checkTransfersQueue();
+        console.log(result)
+      });
